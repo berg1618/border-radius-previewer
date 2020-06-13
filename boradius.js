f43dc33b780d883589f4522a114748ea763e62d1
berg <joselindemberg5@gmail.com>
2020-06-13 13:30:28 -0300

    project almost finished

--- boradius.js
+++ boradius.js
@@ -1,6 +1,19 @@
-const topL = document.getElementById("topL").value;
-
 function calBor(){
-    alert(topL);
-    return true;
+    let topL = document.getElementById("topL").value; 
+    let topR = document.getElementById("topR").value; 
+    let botR = document.getElementById("botR").value;
+    let botL = document.getElementById("botL").value;
+    ans.innerHTML = `border-radius: ${topL}% ${topR}% ${botR}% ${botL}%;`;
+
+    sampleBox.style.borderTopLeftRadius = topL +'%';
+    sampleBox.style.borderTopRightRadius = topR +'%';
+    sampleBox.style.borderBottomLeftRadius = botL +'%';
+    sampleBox.style.borderBottomRightRadius = botR +'%';
+}
+
+function copI(){
+    let text = document.getElementById('ans');
+    text.select();
+    document.execCommand("copy");
+    tip.innerHTML = 'Press Ctrl + C';
 }
